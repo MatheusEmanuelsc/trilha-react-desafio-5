4beaddb4c6204c5926481c6717fc977140ada7c1
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2024-10-01 17:18:01 -0300

    desafio finalizado

--- services/api.js
+++ services/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
-    headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
-    }
-})
+export const api = axios.create({
+  baseURL: 'https://tgcdzgbjaiaanktxrjth.supabase.co/rest/v1',
+  headers: {
+    apikey:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnY2R6Z2JqYWlhYW5rdHhyanRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc4MDg0MDUsImV4cCI6MjA0MzM4NDQwNX0.Zx8UIDlJq38rbL4Mnrj6hilyWUOSb9AWtOzX0anIncU',
+    authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnY2R6Z2JqYWlhYW5rdHhyanRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc4MDg0MDUsImV4cCI6MjA0MzM4NDQwNX0.Zx8UIDlJq38rbL4Mnrj6hilyWUOSb9AWtOzX0anIncU',
+  },
+});
