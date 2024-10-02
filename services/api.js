da207dfd9f430ceb0bbc9221889de95202c2fb9f
MatheusEmanuelsc <101181100+MatheusEmanuelsc@users.noreply.github.com>
2024-10-02 06:34:12 -0300

    ajuste

--- services/api.js
+++ services/api.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://tgcdzgbjaiaanktxrjth.supabase.co/rest/v1',
+  baseURL: '',
   headers: {
     apikey:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnY2R6Z2JqYWlhYW5rdHhyanRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc4MDg0MDUsImV4cCI6MjA0MzM4NDQwNX0.Zx8UIDlJq38rbL4Mnrj6hilyWUOSb9AWtOzX0anIncU',
+      ,
     authorization:
-      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnY2R6Z2JqYWlhYW5rdHhyanRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc4MDg0MDUsImV4cCI6MjA0MzM4NDQwNX0.Zx8UIDlJq38rbL4Mnrj6hilyWUOSb9AWtOzX0anIncU',
+      
   },
 });
+
+//É necessário incluir os tokens para que o projeto funcione corretamente
